e341da265a94768c830711a93d780e675084ed7c
Christian Carter <christian@biggerboatconsulting.com>
2017-02-04 14:18:05 -0800

    fix issues with the base component

--- src/aura/baseCSV/baseCSVHelper.js
+++ src/aura/baseCSV/baseCSVHelper.js
@@ -1,16 +1,15 @@
-({
-	parse : function(component,thing,config) {
-		if(!config.complete) {
-			config.complete = $A.getCallback(function(results,file) {
+({ 
+	parse : function(component,thing) {
+        var complete = $A.getCallback(function(results,file) {
 				 console.log("Parsing complete:", results, file);
 				 var data = results.data.slice(1).filter(function(row){
                  	return row[0];
             	 });
 				 var headers = results.data[0];
 				 component.set('v.rows',data);
-				 component.set('v.headers',data);
+				 component.set('v.headers',headers);
 			})
-		}
-		Papa.parse(thing,config);
+
+        Papa.parse(thing,{complete: complete});
 	}
 })
